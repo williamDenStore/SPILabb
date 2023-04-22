dd6e5c95361b066333890d08f0c03a0d2738a5e3
William Karlsson <william.karlsson1999@gmail.com>
2023-04-22 08:17:22 +0200

    add annotations

--- provider/src/main/java/org/example/provider/addition.java
+++ provider/src/main/java/org/example/provider/addition.java
@@ -1,7 +1,9 @@
 package org.example.provider;
 
 import org.example.service.Calculate;
+import org.example.service.annotation.Operator;
 
+@Operator("addition")
 public class addition implements Calculate {
     @Override
     public int result(int a, int b) {
--- service/src/main/java/module-info.java
+++ service/src/main/java/module-info.java
@@ -1,3 +1,4 @@
 module org.example.service {
     exports org.example.service;
+    exports org.example.service.annotation;
 }
